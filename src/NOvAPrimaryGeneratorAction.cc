58fe89cc3342403735ae3b6171c92c4220c2a388
Pierre Lasorak <pierre.lasorak@gmail.com>
2020-08-17 20:01:07 +0200

    make the default start pos a bit more useful

--- src/NOvAPrimaryGeneratorAction.cc
+++ src/NOvAPrimaryGeneratorAction.cc
@@ -48,15 +48,15 @@ NOvAPrimaryGeneratorAction::NOvAPrimaryGeneratorAction()
   G4int n_particle = 1;
   fParticleGun  = new G4ParticleGun(n_particle);
 
-  // default particle kinematic
   G4ParticleTable* particleTable = G4ParticleTable::GetParticleTable();
   G4String particleName;
-  G4ParticleDefinition* particle = particleTable->FindParticle(particleName="nu_e");
+  G4ParticleDefinition* particle = particleTable->FindParticle(particleName="e-");
   fParticleGun->SetParticleDefinition(particle);
-  fParticleGun->SetParticleMomentumDirection(G4ThreeVector(0.351872229841986,
-                                                           -0.4189879027259112,
-                                                           -0.8370394681467362));
-  fParticleGun->SetParticleEnergy(6.*MeV);
+
+  fParticleGun->SetParticleMomentumDirection(G4ThreeVector(0,0,1));
+  fParticleGun->SetParticlePosition         (G4ThreeVector(0,0,0));
+
+  fParticleGun->SetParticleEnergy(10.*MeV);
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -70,12 +70,6 @@ NOvAPrimaryGeneratorAction::~NOvAPrimaryGeneratorAction()
 
 void NOvAPrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
 {
-  // G4double size = 0.8; 
-  G4double x0 =  1045510.83604937;//size * envSizeXY * (G4UniformRand()-0.5);
-  G4double y0 = 6527.749203333404;//size * envSizeXY * (G4UniformRand()-0.5);
-  G4double z0 = 14775.95125213562;//-0.5 * envSizeZ;
-  
-  fParticleGun->SetParticlePosition(G4ThreeVector(x0,y0,z0));
 
   fParticleGun->GeneratePrimaryVertex(anEvent);
 }
